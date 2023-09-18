f54858ac2e8b9dc843c79f7c7b6c2537e312036d
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-18 21:22:16 +0600

    Create migrations and models

---
+++ database/migrations/2023_09_18_113443_create_resources_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('url');
+            $table->string('description');
+            $table->json('use_cases');
+            $table->uuid('folder_id')
+                ->nullable()
+                ->references('id')
+                ->on('folders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
---
+++ database/migrations/2023_09_18_113738_create_resource_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resource_tag', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('tag_id')
+                ->references('id')
+                ->on('tags');
+            $table->uuid('resource_id')
+                ->references('id')
+                ->on('resources');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resource_tag');
+    }
+};
