d478ae483ed065a404bcc15e5e1917e527f68b4d
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-18 23:05:53 +0600

    Add user seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin', 
+                'email' => 'admin@res-track.com',
+                'password' => 'password'
+            ],
+            [
+                'name' => 'Developer One', 
+                'email' => 'developer.one@res-track.com',
+                'password' => 'password'
+            ],
+            [
+                'name' => 'Developer Two', 
+                'email' => 'developer.two@res-track.com',
+                'password' => 'password'
+            ],
+            [
+                'name' => 'User One', 
+                'email' => 'user.one@res-track.com',
+                'password' => 'password'
+            ],
+            [
+                'name' => 'User Two', 
+                'email' => 'user.two@res-track.com',
+                'password' => 'password'
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
