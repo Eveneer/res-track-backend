9210651fa107ad4a06a092cbc6ff30a35f3b5edb
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-24 02:22:11 +0600

    Work on resource model

--- app/Domain/Resource/Resource.php
+++ app/Domain/Resource/Resource.php
@@ -2,11 +2,48 @@
 
 namespace App\Domain\Resource;
 
+use App\Domain\Comment\Comment;
+use App\Domain\Folder\Folder;
+use App\Domain\User\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Resource extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'url',
+        'description',
+        'use_cases',
+        'user_id',
+        'folder_id',
+    ];
+
+    protected $appends = ['first_comments'];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'resource_id', 'id');
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
+    public function folder(): HasOne
+    {
+        return $this->hasOne(Folder::class, 'folder_id', 'id');
+    }
+
+    public function getFirstCommentsAttribute(): Collection
+    {
+        return $this->comments()->take(10)->get();
+    }
 }
