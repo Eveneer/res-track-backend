3a3f55c42bf328c2c6d4d02847aa963b26804082
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-24 02:55:06 +0600

    Make minor updates to resource search

--- app/Domain/Resource/Actions/GetResources.php
+++ app/Domain/Resource/Actions/GetResources.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Resource\Actions;
 
+use App\Domain\Resource\Resource;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\ActionRequest;
@@ -21,33 +22,35 @@ class GetResources
             'folder_id' => ['sometimes', 'exists:folders,id'],
             'user_id' => ['sometimes', 'exists:users,id'],
             'search' => ['sometimes', 'string', 'max:255'],
-            'page' => ['sometimes', 'integer', 'min:1']
+            'page' => ['sometimes', 'integer', 'min:1'],
+            'per_page' => ['sometimes', 'integer', 'min:1']
         ];
     }
 
     public function handle(array $data): LengthAwarePaginator
     {
-        $query = DB::table('resources');
+        $query = Resource::query();
         if (isset($data['ids'])) {
-            $query = $query->whereIn('id', $data['ids']);
+            $query->whereIn('id', $data['ids']);
         } else {
             if (isset($data['user_id'])) {
-                $query = $query->whereUserId($data['user_id']);
+                $query->whereUserId($data['user_id']);
             }
 
             if (isset($data['folder_id'])) {
-                $query = $query->whereFolderId($data['folder_id']);
+                $query->whereFolderId($data['folder_id']);
             }
 
             if (isset($data['search'])) {
-                $query = $query->where('name', 'like', "%" . $data['search'] . "%");
-                $query = $query->where('description', 'like', "%" . $data['search'] . "%");
-                $query = $query->where('url', 'like', "%" . $data['search'] . "%");
-                $query = $query->orWhereRaw('use_cases->"$[*].*" like ?', "%" . $data['search'] . "%");
+                $query->where('name', 'like', "%" . $data['search'] . "%");
+                $query->where('description', 'like', "%" . $data['search'] . "%");
+                $query->where('url', 'like', "%" . $data['search'] . "%");
+                $query->orWhereRaw('use_cases->"$[*].*" like ?', "%" . $data['search'] . "%");
+                // enable search using tags
             }
         }
 
-        return $query->paginate(5, ['*'], $data['page'] ?? 1);
+        return $query->paginate($data['per_page'] ?? 5, ['*'], $data['page'] ?? 1);
     }
 
     public function asController(ActionRequest $request): LengthAwarePaginator
