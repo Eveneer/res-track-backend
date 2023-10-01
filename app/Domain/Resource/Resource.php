a265d2e5ce28d8322ee7e7061f6417c17206d669
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-01 13:08:34 +0600

    Add tag relationship to resource

--- app/Domain/Resource/Resource.php
+++ app/Domain/Resource/Resource.php
@@ -4,11 +4,13 @@ namespace App\Domain\Resource;
 
 use App\Domain\Comment\Comment;
 use App\Domain\Folder\Folder;
+use App\Domain\Tag\Tag;
 use App\Domain\User\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -40,6 +42,18 @@ class Resource extends Model
     public function folder(): HasOne
     {
         return $this->hasOne(Folder::class, 'folder_id', 'id');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Tag::class, 
+            'resource_tag', 
+            'resource_id', 
+            'tag_id', 
+            'id', 
+            'id'
+        );
     }
 
     public function getFirstCommentsAttribute(): Collection
