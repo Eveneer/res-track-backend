9ae283ad0ee02a640d9c5e33a6f506a98aa1f14d
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-01 14:14:36 +0600

    Add user relationship to user invite

--- app/Domain/UserInvite/UserInvite.php
+++ app/Domain/UserInvite/UserInvite.php
@@ -2,11 +2,20 @@
 
 namespace App\Domain\UserInvite;
 
+use App\Domain\User\User;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserInvite extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = ['email'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'email', 'email');
+    }
 }
