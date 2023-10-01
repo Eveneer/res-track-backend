e1a447ef88e978e44535475f1706d736d9adb677
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-01 13:27:14 +0600

    Update tag modle

--- app/Domain/Tag/Tag.php
+++ app/Domain/Tag/Tag.php
@@ -2,11 +2,27 @@
 
 namespace App\Domain\Tag;
 
+use App\Domain\Resource\Resource;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = ['name', 'tag-key'];
+
+    public function resources(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Resource::class, 
+            'resource_tag', 
+            'tag_id', 
+            'resource_id', 
+            'id', 
+            'id'
+        );
+    }
 }
