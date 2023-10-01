9dd80acdb6516c56724dfdd740dac76bd07bc072
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-01 13:18:35 +0600

    Update tag migration

--- database/migrations/2023_09_18_113458_create_tags_table.php
+++ database/migrations/2023_09_18_113458_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('tag');
+            $table->string('name');
             $table->string('tag_key');
             $table->timestamps();
         });
